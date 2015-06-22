782308e003afee2df5e354b82a0a4830d81cc244
Murphy Berzish <murphy.berzish@gmail.com>
2015-06-22 19:25:50 -0400

    new hierarchy

--- arraysafety/ArraySafetyChecker.java
+++ arraysafety/ArraySafetyChecker.java
@@ -4,7 +4,9 @@ import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.qual.TypeQualifiers;
 
 import org.checkerframework.checker.arraysafety.qual.UnknownArraySafety;
+import org.checkerframework.checker.arraysafety.qual.SafeArrayIndex;
+import org.checkerframework.checker.arraysafety.qual.UnsafeArrayIndex;
 import org.checkerframework.checker.arraysafety.qual.ArraySafetyBottom;
 
-@TypeQualifiers({UnknownArraySafety.class,/*SafeArrayIndex.class,UnsafeArrayIndex.class,*/ArraySafetyBottom.class})
+@TypeQualifiers({UnknownArraySafety.class,SafeArrayIndex.class,UnsafeArrayIndex.class,ArraySafetyBottom.class})
     public class ArraySafetyChecker extends BaseTypeChecker { }
