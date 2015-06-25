8f05255f1d308900c5a8d3e7aeb4fcb73e4eec6f
Murphy Berzish <murphy.berzish@gmail.com>
2015-06-25 17:31:01 -0400

    use value subchecker; this crashes InferTypeArgs3.java in all-systems

--- arraysafety/ArraySafetyChecker.java
+++ arraysafety/ArraySafetyChecker.java
@@ -13,4 +13,11 @@ import org.checkerframework.checker.arraysafety.qual.UnsafeArrayIndex;
 import org.checkerframework.checker.arraysafety.qual.ArraySafetyBottom;
 
 @TypeQualifiers({UnknownArraySafety.class,SafeArrayIndex.class,UnsafeArrayIndex.class,ArraySafetyBottom.class})
-public class ArraySafetyChecker extends BaseTypeChecker { }
+public class ArraySafetyChecker extends BaseTypeChecker {
+    protected LinkedHashSet<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
+	LinkedHashSet<Class<? extends BaseTypeChecker>> subcheckers = new LinkedHashSet<>();
+	subcheckers.addAll(super.getImmediateSubcheckerClasses());
+	subcheckers.add(ValueChecker.class);
+	return subcheckers;
+    }
+}
