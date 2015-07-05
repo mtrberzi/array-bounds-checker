8f56d3ce302bc767f531b77a821cb595ff181015
Murphy Berzish <murphy.berzish@gmail.com>
2015-07-05 15:40:05 -0400

    never mind

--- tests/arraysafety/Flow.java
+++ tests/arraysafety/Flow.java
@@ -10,9 +10,26 @@ public class Flow {
 	if (idx >= 0 && idx < array.length) {
 	    array[idx] = 42;
 	} else {
-	    //: error: (array.access.unsafe)
+	    //:: error: (array.access.unsafe)
 	    array[idx] = 9001;
 	}
+    }
+
+    int safeForLoopAccess(int[] array) {
+	int sum = 0;
+	for (int i = 0; i < array.length; ++i) {
+	    sum += array[i];
+	}
+	return sum;
+    }
+
+    int unsafeForLoopAccess(int[] array) {
+	int sum = 0;
+	for (int i = 0; i <= array.length; ++i) {
+	    //:: error: (array.access.unsafe)
+	    sum += array[i];
+	}
+	return sum;
     }
     
 }
